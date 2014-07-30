821268ccd173a8b38947fe5d7484d018130627fe
zeekvfu <wzmindlog@gmail.com>
2014-07-31 00:38:25 +0800

    big changes on download URL

--- crawler.py
+++ crawler.py
@@ -46,8 +46,9 @@ begin = int(input('请输入开始的期数：'))
 end   = int(input('请输入结束的期数：'))
 
 # data-url="http://traffic.libsyn.com/itgonglun/IT__ep1_-_iPad_Air_iPad_mini_2_Mavericks_GM.mp3"
-rule = "data-url=\"http://traffic\.libsyn\.com/itgonglun/IT__ep\d+_-_.*\.mp3\s*\""
-regex = re.compile(rule)
+regex0 = re.compile("data-url=\"http://traffic\.libsyn\.com/itgonglun/IT__ep\d+_-_.*\.mp3\s*\"")
+# data-url="http://cdn.itgonglun.com/itgonglun/ep88.mp3"
+regex1 = re.compile("data-url=\"http://cdn\.itgonglun\.com/itgonglun/ep\d+\.mp3\s*\"")
 
 log_file = 'url.log'
 with open(log_file, 'at') as logger:
@@ -62,11 +63,21 @@ stdout_handler.setFormatter(formatter)
 logging.getLogger().addHandler(stdout_handler)
 
 for index in range(begin, end+1):
-	url = parse_download_url(index, regex)
-	if url is None:
-		continue
-	logging.info(str(index) + '\t' + url)
-	multi_thread_continous_download(url)
+    if ((index in range(1,70)) or (index in range(71,78)) or (index in range(79,82)) or (index == 83) or (index in range(85,88))):
+        url = parse_download_url(index, regex0)
+    elif (index >= 88):
+        url = parse_download_url(index, regex1)
+    # 没有第 70 期
+    elif (index == 70):
+        continue
+    elif (index == 78):
+        url="http://traffic.libsyn.com/preview/itgonglun/IT__ep78_-_digital_native.mp3"
+    elif (index == 82):
+        url="http://traffic.libsyn.com/itgonglun/IT__82_-_new_iPhone_ad_wearable_and_ubicomp.mp3"
+    elif (index == 84):
+        url="http://cdn.lizhi.fm/audio/2014/07/10/12892403592174214_hd.mp3"
+    logging.info(str(index) + '\t' + url)
+    multi_thread_continous_download(url)
 
 logging.shutdown()
 
@@ -74,7 +85,15 @@ logging.shutdown()
 
 
 # 比较特殊的 URL：
-# 58			http://www.itgonglun.com/episodes/59-1
+# 58		http://www.itgonglun.com/episodes/59-1
+
+# 比较特殊的下载链接：
+# 70		无
+# 78		http://traffic.libsyn.com/preview/itgonglun/IT__ep78_-_digital_native.mp3
+# 82		http://traffic.libsyn.com/itgonglun/IT__82_-_new_iPhone_ad_wearable_and_ubicomp.mp3
+# 84		http://cdn.lizhi.fm/audio/2014/07/10/12892403592174214_hd.mp3
+# 88		http://cdn.itgonglun.com/itgonglun/ep88.mp3
+# 89		http://cdn.itgonglun.com/itgonglun/ep89.mp3
 
 
 
